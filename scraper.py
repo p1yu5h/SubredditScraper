8e12cdc8cfcbb862fe4a72641d8fcb9e33360ab7
p1yu5h <piyush.satija@gmail.com>
2019-08-11 19:23:20 +0530

    Add Check to see if subreddit exists	before crawling.

--- scraper.py
+++ scraper.py
@@ -1,21 +1,33 @@
 #! usr/bin/python
 import praw
+from prawcore import NotFound
 import pandas as pd
 import datetime as dt
 import config
 import csv
 
-reddit = praw.Reddit(client_id=config.client_id,
-                     client_secret=config.client_secret,
-                     user_agent=config.user_agent,
-                     username=config.username,
-                     password=config.password)
+reddit = praw.Reddit(
+    client_id=config.client_id,
+    client_secret=config.client_secret,
+    user_agent=config.user_agent,
+    username=config.username,
+    password=config.password,
+)
 
 sub = raw_input("Subreddit to scrape: ")
-subreddit = reddit.subreddit(sub)
 
-with open('./csv/{}.csv'.format(sub), mode='w') as file:
-    writer = csv.writer(file, quotechar='"', quoting=csv.QUOTE_NONE)
-    for submission in subreddit.top(limit=200):
+def sub_exists(sub):
+    try:
+        reddit.subreddits.search_by_name(sub, exact=True)
+        return True
+    except NotFound:
+        print("Error: Subreddit r/{} Not Found...".format(sub))
+        return False
+
+if sub_exists(sub):
+    subreddit = reddit.subreddit(sub)
+    with open("./csv/{}.csv".format(sub), mode="w") as file:
+        writer = csv.writer(file, quotechar='"', quoting=csv.QUOTE_NONE)
+        for submission in subreddit.top(limit=200):
             writer.writerow([submission.id, submission.url])
-    print("Done!")
+        print("Done!")
